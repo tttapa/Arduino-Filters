61822db07ec1ea956f150e750371263cdb9916be
Pieter Pas <pieter.pas@outlook.com>
2019-11-26 19:47:40 +0100

    Documentation SMA

--- src/Filters/SMA.hpp
+++ src/Filters/SMA.hpp
@@ -41,9 +41,37 @@ T round_div(T val) {
     return round_div_helper<N, T>::div(val);
 }
 
+/// @addtogroup Filters
+/// @{
+
+/** 
+ * @brief   Simple Moving Average filter.
+ * 
+ * @f[
+ * y[n] = \frac{1}{N} \sum_{i=0}^{N-1}x[n-i]
+ * @f]
+ * 
+ * @see     https://tttapa.github.io/Pages/Mathematics/Systems-and-Control-Theory/Digital-filters/Simple%20Moving%20Average/Simple-Moving-Average.html
+ * 
+ * @tparam  N
+ *          The number of samples to average.
+ * @tparam  input_t
+ *          The type of the input (and output) of the filter.
+ * @tparam  sum_t
+ *          The type to use for the accumulator, must be large enough to fit
+ *          N times the maximum input value.
+ */
 template <uint8_t N, class input_t = uint16_t, class sum_t = uint32_t>
 class SMA {
   public:
+    /**
+     * @brief   Update the internal state with the new input @f$ x[n] @f$ and
+     *          return the new output @f$ y[n] @f$.
+     * 
+     * @param   input 
+     *          The new input @f$ x[n] @f$.
+     * @return  The new output @f$ y[n] @f$.
+     */
     input_t operator()(input_t input) {
         sum -= previousInputs[index];
         sum += input;
@@ -53,13 +81,10 @@ class SMA {
         return round_div<N>(sum);
     }
 
-    static_assert(
-        sum_t(0) < sum_t(-1), // Check that `sum_t` is an unsigned type
-        "Error: sum data type should be an unsigned integer, otherwise, "
-        "the rounding operation in the return statement is invalid.");
-
   private:
     uint8_t index = 0;
     AH::Array<input_t, N> previousInputs = {};
     sum_t sum = 0;
 };
+
+/// @}
